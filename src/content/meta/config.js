e60a9fc419a3a5790c7c98f34bdd5ef1e0141452
greglobinski <greglobinski@gmail.com>
2018-08-06 08:47:39 +0200

    fix(content): copy content for equipped from newbase/addons

--- src/content/meta/config.js
+++ src/content/meta/config.js
@@ -4,6 +4,15 @@ const base = {
 };
 
 const config = {
+  /* meta tags */
+  siteTitle: `${base.name} - a series of GatsbyJS starters`,
+  siteTitlePostfix: ` - ${base.name}`,
+  siteDescription: `${
+    base.name
+  } is a series starters: Minimal, Equipped, Themed, Website, Blog.`,
+  siteImage: 'preview.jpg',
+  siteLanguage: 'en',
+
   /* site header */
   headerTitle: `${base.name}`,
   headerSubTitle: 'a series of starters for GatsbyJS',
