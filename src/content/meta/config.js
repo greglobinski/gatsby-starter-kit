c8c914563c775fbc0f24fc670755e761042b9435
greglobinski <greglobinski@gmail.com>
2018-08-06 07:11:07 +0200

    Update config object

--- src/content/meta/config.js
+++ src/content/meta/config.js
@@ -1,17 +1,8 @@
 const base = {
-  name: 'Gatsby Starter Kit',
-  url: 'https://greglobinski.github.io/gatsby-starter-kit/',
-  author: 'greg lobinski'
+  name: 'Gatsby Starter Kit'
 };
 
 const config = {
-  // meta tags
-  siteTitle: `${base.name} - a series of starters for GatsbyJS`,
-  siteTitlePostfix: ` - ${base.name}`,
-  siteDescription: `This is a series of GatsbyJS starters: Minimal, Equipped, Themed, Website, Blog`,
-  siteImage: 'preview.jpg',
-  siteLanguage: 'en',
-
   // url
   siteUrl: base.url,
   // pathPrefix: '',
