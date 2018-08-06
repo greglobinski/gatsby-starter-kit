8ab0af4d72e57374bfa74a7cdf9fa977bd9e88dd
greglobinski <greglobinski@gmail.com>
2018-08-06 22:14:52 +0200

    Copy gatsby-config and gatsby-node from newbase

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,9 +14,54 @@ module.exports = {
         path: `${__dirname}/src/content/parts/`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/content/pages/`,
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-resolve-src`,
     `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 690,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-responsive-iframe`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          {
+            resolve: 'gatsby-remark-emojis',
+            options: {
+              // Deactivate the plugin globally (default: true)
+              active: true,
+              // Add a custom css class
+              class: 'emoji-icon',
+              // Select the size (available size: 16, 24, 32, 64)
+              size: 64,
+              // Add custom styles
+              styles: {
+                display: 'inline',
+                margin: '0',
+                'margin-top': '1px',
+                position: 'relative',
+                top: '5px',
+                width: '25px',
+              },
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -24,5 +69,6 @@ module.exports = {
         trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
     },
+    `gatsby-plugin-emotion`,
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,119 @@
 const path = require('path');
+const Promise = require('bluebird');
+
+const { createFilePath } = require(`gatsby-source-filesystem`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MarkdownRemark`) {
+    const filePath = createFilePath({ node, getNode });
+
+    const separtorIndex = ~filePath.indexOf('--') ? filePath.indexOf('--') : 0;
+    const slugStart = separtorIndex ? separtorIndex + 2 : 0;
+
+    const slug = `${separtorIndex ? '/' : ''}${filePath.substring(slugStart)}`;
+    const identifier = slug.replace(/\//g, '');
+    const prefix = separtorIndex ? filePath.substring(1, separtorIndex) : '';
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+    createNodeField({
+      node,
+      name: `identifier`,
+      value: identifier,
+    });
+    createNodeField({
+      node,
+      name: `prefix`,
+      value: prefix,
+    });
+  }
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return new Promise((resolve, reject) => {
+    const pageTemplate = path.resolve('./src/templates/PageTemplate.js');
+
+    resolve(
+      graphql(`
+        {
+          allMarkdownRemark(
+            filter: { fileAbsolutePath: { regex: "//pages//" } }
+            sort: { fields: [fields___prefix], order: DESC }
+            limit: 1000
+          ) {
+            edges {
+              node {
+                fileAbsolutePath
+                fields {
+                  slug
+                  identifier
+                  prefix
+                }
+                frontmatter {
+                  title
+                  categories
+                }
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+
+        const items = result.data.allMarkdownRemark.edges;
+
+        const categorySet = new Set();
+
+        // Create category list
+        items.forEach(edge => {
+          const {
+            node: {
+              frontmatter: { categories },
+            },
+          } = edge;
+
+          if (categories) {
+            categories.forEach(category => {
+              categorySet.add(category);
+            });
+          }
+        });
+
+        // create pages
+        const pages = items.filter(item =>
+          /pages/.test(item.node.fileAbsolutePath)
+        );
+        pages.forEach(({ node }) => {
+          const slug = node.fields.slug;
+
+          createPage({
+            path: slug,
+            component: pageTemplate,
+            context: {
+              slug,
+            },
+          });
+        });
+      })
+    );
+  });
+};
 
 exports.onCreateWebpackConfig = ({
   stage,
   rules,
   loaders,
   plugins,
-  actions
+  actions,
 }) => {
   switch (stage) {
     case 'build-javascript':
@@ -15,10 +123,10 @@ exports.onCreateWebpackConfig = ({
             {
               test: /\.yaml$/,
               include: path.resolve('data'),
-              loader: 'yaml'
-            }
-          ]
-        }
+              loader: 'yaml',
+            },
+          ],
+        },
       });
   }
 };
