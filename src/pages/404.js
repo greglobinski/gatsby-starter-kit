4d1b3ce3ec1a7964e6450c15b70ff7309faa1eb5
greglobinski <greglobinski@gmail.com>
2018-08-06 18:48:48 +0200

    Change locla components over imported

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from 'starterComponents/Layout';
-import Article from 'starterComponents/Article';
-import Heading from 'starterComponents/Heading';
-import Bodytext from 'starterComponents/Bodytext';
+import {
+  Layout,
+  Article,
+  Bodytext,
+  Heading,
+  Seo,
+} from 'gatsby-starter-kit-themes/dist/default';
+
+import config from 'content/meta/config';
+import menu from 'content/meta/menu';
 
 const NotFoundPage = props => {
   const {
     data: {
       notFound: { html: notFoundHTML },
+      footerLinks: { html: footerLinksHTML },
+      copyrightNote: { html: copyrightNoteHTML },
     },
   } = props;
 
+  const { headerTitle, headerSubTitle } = config;
+
   return (
-    <Layout>
+    <Layout
+      footerLinks={footerLinksHTML}
+      copyrightNote={copyrightNoteHTML}
+      headerTitle={headerTitle}
+      headerSubTitle={headerSubTitle}
+      menu={menu}
+    >
       <Article>
         <Heading title="NOT FOUND" />
         <Bodytext html={notFoundHTML} />
       </Article>
+      <Seo />
     </Layout>
   );
 };
@@ -29,6 +46,16 @@ export const query = graphql`
   query {
     notFound: markdownRemark(
       fileAbsolutePath: { regex: "/content/parts/notFound/" }
+    ) {
+      html
+    }
+    footerLinks: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/footerLinks/" }
+    ) {
+      html
+    }
+    copyrightNote: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/copyrightNote/" }
     ) {
       html
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from 'starterComponents/Layout';
-import Article from 'starterComponents/Article';
-import Bodytext from 'starterComponents/Bodytext';
+import {
+  Layout,
+  Article,
+  Bodytext,
+  Seo,
+} from 'gatsby-starter-kit-themes/dist/default';
+
+import config from 'content/meta/config';
+import menu from 'content/meta/menu';
 
 const IndexPage = props => {
   const {
     data: {
       welcome: { html: welcomeHTML },
+      footerLinks: { html: footerLinksHTML },
+      copyrightNote: { html: copyrightNoteHTML },
     },
   } = props;
 
+  const { headerTitle, headerSubTitle } = config;
+
   return (
-    <Layout>
+    <Layout
+      footerLinks={footerLinksHTML}
+      copyrightNote={copyrightNoteHTML}
+      headerTitle={headerTitle}
+      headerSubTitle={headerSubTitle}
+      menu={menu}
+    >
       <Article>
         <Bodytext html={welcomeHTML} />
       </Article>
+      <Seo config={config} />
     </Layout>
   );
 };
@@ -27,6 +44,16 @@ export const query = graphql`
   query {
     welcome: markdownRemark(
       fileAbsolutePath: { regex: "/content/parts/welcome/" }
+    ) {
+      html
+    }
+    footerLinks: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/footerLinks/" }
+    ) {
+      html
+    }
+    copyrightNote: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/copyrightNote/" }
     ) {
       html
     }
