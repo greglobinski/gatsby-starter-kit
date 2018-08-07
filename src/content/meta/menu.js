be781ab66ce972d550ac6dc6c3d6e09c326c2243
greglobinski <greglobinski@gmail.com>
2018-08-07 12:48:54 +0200

    Update content for website

--- src/content/meta/menu.js
+++ src/content/meta/menu.js
@@ -1,11 +1,9 @@
 import FaHome from 'react-icons/lib/fa/home';
 import FaInfo from 'react-icons/lib/fa/info';
-import FaEnvelope from 'react-icons/lib/fa/envelope';
 
 const menu = [
   { label: 'Home', to: '/', icon: FaHome },
-  { label: 'About', to: '/about', icon: FaInfo },
-  { label: 'Contact', to: '/contact', icon: FaEnvelope }
+  { label: 'About', to: '/about', icon: FaInfo }
 ];
 
 export default menu;
--- src/content/pages/2--contact/index.md
+++
@@ -1,5 +0,0 @@
----
-title: Contact
----
-
-Nam porttitor, erat sit amet venenatis luctus, augue libero ultrices quam, ut congue nisi risus eu purus.
