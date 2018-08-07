1058fcbfb9586ca41b28d11e0a6a874ed7b328f8
greglobinski <greglobinski@gmail.com>
2018-08-07 16:28:37 +0200

    Add blog link to menu

--- src/content/meta/menu.js
+++ src/content/meta/menu.js
@@ -1,8 +1,10 @@
 import FaHome from 'react-icons/lib/fa/home';
 import FaInfo from 'react-icons/lib/fa/info';
+import FaBook from 'react-icons/lib/fa/book';
 
 const menu = [
   { label: 'Home', to: '/', icon: FaHome },
+  { label: 'Blog', to: '/blog', icon: FaBook },
   { label: 'About', to: '/about', icon: FaInfo }
 ];
 
