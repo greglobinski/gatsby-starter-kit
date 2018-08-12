98c48d321aa4696837411a6825fc175db53a5c37
greglobinski <greglobinski@gmail.com>
2018-08-12 14:29:20 +0200

    Update content-for-website

--- src/content/meta/menu.js
+++ src/content/meta/menu.js
@@ -1,8 +1,10 @@
 import HomeIcon from 'react-feather/dist/icons/home';
+import FolderIcon from 'react-feather/dist/icons/folder';
 import InfoIcon from 'react-feather/dist/icons/info';
 
 const menu = [
   { label: 'Home', to: '/', icon: HomeIcon },
+  { label: 'Catalog', to: '/catalog', icon: FolderIcon }
   { label: 'About', to: '/about', icon: InfoIcon }
 ];
 
