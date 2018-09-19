9a2fdaf1e035b53f7732682c604469087164cf76
greglobinski <greglobinski@gmail.com>
2018-09-19 14:47:31 +0200

    fix(code/side-blog): update style of emojis

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,20 +67,15 @@ module.exports = {
           {
             resolve: 'gatsby-remark-emojis',
             options: {
-              // Deactivate the plugin globally (default: true)
               active: true,
-              // Add a custom css class
               class: 'emoji-icon',
-              // Select the size (available size: 16, 24, 32, 64)
-              size: 64,
-              // Add custom styles
+              size: 32,
               styles: {
                 display: 'inline',
                 margin: '0',
-                'margin-top': '1px',
                 position: 'relative',
-                top: '5px',
-                width: '25px',
+                top: '3px',
+                width: '20px',
               },
             },
           },
