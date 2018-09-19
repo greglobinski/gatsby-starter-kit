e0a5749e86928b0a35834cf1e78dd0183cfd2369
greglobinski <greglobinski@gmail.com>
2018-09-19 14:14:57 +0200

    fix(code/side-glob): update Heading prop on Home

--- src/pages/index.js
+++ src/pages/index.js
@@ -51,8 +51,14 @@ const IndexPage = props => {
               })
             }
           >
-            <img src={logo} alt={siteTitle} className={logoStyle} />
-            <Heading title={title} home={true} />
+            <Heading special={true}>
+              <img src={logo} alt={`${title} ${subTitle}`} />
+              <h1>
+                <small>{preTitle}</small>
+                {title}
+                <span>{subTitle}</span>
+              </h1>
+            </Heading>
             <Bodytext html={homeHTML} />
           </Article>
         )}
@@ -77,6 +83,8 @@ export const query = graphql`
       html
       frontmatter {
         title
+        preTitle
+        subTitle
       }
     }
     footerLinks: markdownRemark(
