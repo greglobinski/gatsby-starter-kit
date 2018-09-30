8c18442e556b9564a77c9cd52a6ef6647b08c870
greglobinski <greglobinski@gmail.com>
2018-09-30 11:11:37 +0200

    feat(code/scroll-zoom): wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,24 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { css } from 'emotion';
 
-import '@react-website-themes/default/styles/variables';
-import '@react-website-themes/default/styles/global';
+//import '@react-website-themes/job-interview/styles/variables';
+import '../../../../mynpms/react-website-themes/packages/job-interview/src/styles/variables';
+import '../../../../mynpms/react-website-themes/packages/job-interview/src//styles/global';
 
-import Article from '@react-website-themes/default/components/Article';
-import Bodytext from '@react-website-themes/default/components/Bodytext';
-import Branding from '@react-website-themes/default/components/Branding';
-import Footer from '@react-website-themes/default/components/Footer';
-import Header from '@react-website-themes/default/components/Header';
-import Heading from '@react-website-themes/default/components/Heading';
-import Layout from '@react-website-themes/default/components/Layout';
-import Menu from '@react-website-themes/default/components/Menu';
-import Seo from '@react-website-themes/default/components/Seo';
+import Screens from '../../../../mynpms/react-website-themes/packages/job-interview/src/components/Screens';
+import Screen from '../../../../mynpms/react-website-themes/packages/job-interview/src/components/Screen';
+import Seo from '../../../../mynpms/react-website-themes/packages/job-interview/src/components/Seo';
+
+import avatar from '../content/images/avatar.jpg';
 
 import config from 'content/meta/config';
-import menuItems from 'content/meta/menu';
 
 const IndexPage = props => {
   const {
     data: {
       footerLinks: { html: footerLinksHTML },
       copyright: { html: copyrightHTML },
-      welcome: {
-        html: welcomeHTML,
-        frontmatter: { title: welcomeTitle },
-      },
     },
   } = props;
 
@@ -38,24 +31,39 @@ const IndexPage = props => {
     siteLanguage,
   } = config;
 
+  const screens = [
+    { id: 1, headline: `Hi, I'm Greg`, text: 'subtext', background: '#fff' },
+    {
+      id: 2,
+      headline: `I'm a front-end developer`,
+      text: 'subtext',
+      background: '#BCE4F2',
+    },
+    {
+      id: 3,
+      headline: `I used to be an art director and web designer`,
+      text: 'subtext',
+      background: '#FCC4D9',
+    },
+    {
+      id: 4,
+      headline: 'Thank you.',
+      text: 'subtext',
+      background: '#D69E7A',
+      avatar: avatar,
+    },
+  ].reverse();
+
   return (
-    <Layout>
-      <Header>
-        <Branding title={headerTitle} subTitle={headerSubTitle} />
-        <Menu items={menuItems} />
-      </Header>
-      <Article>
-        <Heading title={welcomeTitle} />
-        <Bodytext html={welcomeHTML} />
-      </Article>
-      <Footer links={footerLinksHTML} copyright={copyrightHTML} />
+    <>
+      <Screens screens={screens} />
       <Seo
         url={siteUrl}
         language={siteLanguage}
         title={siteTitle}
         description={siteDescription}
       />
-    </Layout>
+    </>
   );
 };
 
@@ -63,14 +71,6 @@ export default IndexPage;
 
 export const query = graphql`
   query {
-    welcome: markdownRemark(
-      fileAbsolutePath: { regex: "/content/parts/welcome/" }
-    ) {
-      html
-      frontmatter {
-        title
-      }
-    }
     footerLinks: markdownRemark(
       fileAbsolutePath: { regex: "/content/parts/footerLinks/" }
     ) {
