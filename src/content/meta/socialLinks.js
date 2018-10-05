bed5af3611dad1b7c201acdf5956a3025bab6b8a
greglobinski <greglobinski@gmail.com>
2018-10-05 13:35:08 +0200

    fix(content/job-interview): move socialLinks to content/meta

---
+++ src/content/meta/socialLinks.js
@@ -0,0 +1,11 @@
+import GithubIcon from 'react-feather/dist/icons/github';
+import TwitterIcon from 'react-feather/dist/icons/twitter';
+import MailIcon from 'react-feather/dist/icons/mail';
+
+const socialLinks = [
+  { url: 'https://github.com/greglobinski', icon: GithubIcon },
+  { url: 'https://twitter.com/greglobinski', icon: TwitterIcon },
+  { url: 'mailto:greglobinski@gmail.com', icon: MailIcon },
+];
+
+export default socialLinks;
