3c4404bb88017550cfd87581fcb1a6eb949609cc
greglobinski <greglobinski@gmail.com>
2018-10-12 20:54:03 +0200

    chore(content/new-minimal): merge old content/minimal into a new one

---
+++ src/content/meta/config.js
@@ -0,0 +1,16 @@
+const base = {
+  name: 'Gatsby Starter Kit',
+  url: 'https://github.com/greglobinski/gatsby-starter-kit',
+};
+
+const config = {
+  /* site header */
+  headerTitle: `${base.name}`,
+  headerSubTitle: 'a set of starters for Gatsby.js',
+
+  /* url */
+  siteUrl: base.url,
+  // pathPrefix: '',
+};
+
+module.exports = config;
---
+++ src/content/parts/notFound.md
@@ -0,0 +1,5 @@
+---
+name: notFound
+---
+
+Quisque lacus augue, mollis non mollis et, ullamcorper in purus. Morbi et sem orci.
