572249e8f0c7042511f8d4ff311e8ee30f3a4eab
greglobinski <greglobinski@gmail.com>
2018-10-13 11:45:23 +0200

    chore(code/new-themed): merga and squash old code/themed into a new one

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const config = require('./src/content/meta/config');
+
+module.exports = {
+  siteMetadata: {
+    title: config.siteTitle,
+    siteUrl: config.siteUrl,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `parts`,
+        path: `${__dirname}/src/content/parts/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-resolve-src`,
+    `gatsby-plugin-catch-links`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-emotion`,
+  ],
+};
